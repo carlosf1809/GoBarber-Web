33fdefba197fee6e630dd313a597e130cd506f11
carlosf1809 <carlosmoreiraaraujo18@gmail.com>
2020-12-23 01:37:28 -0300

    up

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import { FiArrowLeft, FiMail,FiUser, FiLock } from 'react-icons/fi'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
+import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import logoImg from '../../assets/logo.svg'
 
 import {Container, Content, Background} from './styles'
 
-const SignUp: React.FC = () => (
+const SignUp: React.FC = () => {
+    const handleSubmit = useCallback(async (dados: object ) => {
+        try{
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatótorio'),
+                email: Yup.string().required('E-mail obrigatótorio').email('Digite um e-mail válido'),
+                password: Yup.string().min(6, 'Senha min. 6 digitos'),
+                
+            });
 
-    <Container>
+            await schema.validate(dados, {abortEarly:false});
+        } catch(error) {
+            console.log(error)
+        }
+    },[]);
+    return (
+        <Container>
         <Background></Background>
         <Content>
             <img src={logoImg} alt="GoBarber"/>
 
-            <form>
+            <Form onSubmit={handleSubmit}>
                 <h1>Faça seu cadastro</h1>
 
                 <Input name="name" icon={FiUser} placeholder="Nome"/>
@@ -25,7 +41,7 @@ const SignUp: React.FC = () => (
 
                 <Button type="submit">Cadastrar</Button>
 
-            </form>
+            </Form>
 
             <a href="#">
                     <FiArrowLeft/>    
@@ -34,7 +50,7 @@ const SignUp: React.FC = () => (
 
         </Content>
     </Container>
-
-);
+    );
+}
 
 export default SignUp;
